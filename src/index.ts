7699c8ec81d8fa3b24c8b9fb507ce5326731d397
jean sidharta <jeansidharta@gmail.com>
2020-06-14 17:09:08 -0300

    feat: basic socket data setup

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,15 @@ const port = process.env.PORT;
 
 if (!port) throw Error('Please set PORT at .env file');
 
-const server = net.createServer();
+const server = net.createServer(socket => {
+	socket.on('data', chunk => {
+		const string = chunk.toString('utf8');
+		console.log(`chunk '${string}'`);
+	});
 
-server.on('connection', (...args: any[]) => {
-	console.log(args);
+	socket.on('end', (...args) => {
+		console.log(`end ${args}`);
+	});
 });
 
-server.listen(port, () => console.log('Sockets are open!'));
+server.listen(port, () => console.log(`Listening on port ${port}`));
