deda54ca48a08f7e76cec2a9b53a2de0565e7cc3
jean sidharta <jeansidharta@gmail.com>
2020-06-14 16:37:19 -0300

    feat: basic socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+const net = require('net');
 
-console.log('hellor backend');
+const server = net.createServer();
+
+server.on('connection', (...args: any[]) => {
+	console.log(args);
+});
+
+server.listen(8080);
